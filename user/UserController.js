f871f209731089e5292bb42c33188987f21b2a4e
Nithin Pingili <nithinp300@gmail.com>
2019-12-24 19:29:18 -0600

    created GET single user using id endpoint

--- user/UserController.js
+++ user/UserController.js
@@ -26,4 +26,13 @@ router.get('/', function(req, res) {
   });
 });
 
+// gets a single user from the database
+router.get('/:id', function(req, res) {
+  User.findById(req.params.id, function (err, user) {
+    if (err) return res.status(500).send("There was a problem finding the user.");
+    if (!user) return res.status(404).send("No user found.");
+    res.status(200).send(user);
+  });
+});
+
 module.exports = router;
